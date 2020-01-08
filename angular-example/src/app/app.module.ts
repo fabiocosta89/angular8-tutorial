d7f9eee562eff912057eee97b2b9f652f005e319
Fábio Costa <fabio@Fabios-MacBook-Pro.local>
2020-01-08 13:47:39 +0100

    - add fake database; - using API to load fake database on Home page; - show error alert on exception;

--- angular-example/src/app/app.module.ts
+++ angular-example/src/app/app.module.ts
@@ -6,19 +6,36 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatProgressSpinnerModule } from '@angular/material';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  @NgModule({
+    declarations: [
+      AppComponent,
+      HomeComponent,
+      AboutComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatButtonModule,
+      MatCardModule,
+      MatProgressSpinnerModule
+    ],
+    exports: [MatToolbarModule,
+      MatIconModule,
+      MatButtonModule,
+      MatCardModule,
+      MatProgressSpinnerModule],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
